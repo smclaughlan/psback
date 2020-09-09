cd5256236577ccc34fc005b8ba8e818de916c431
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-09-08 17:56:13 -0700

    cors

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(cors());
+app.use(cors());
 app.use('/', indexRouter);
 
 module.exports = app;
