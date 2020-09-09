d57372303863c295564c82307ea42cfc236271ba
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-09-08 17:33:33 -0700

    res.end instead of res.json the comment on new post

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.post("/comments", cors(), async function (req, res) {
     body
   })
 
-  res.json({ comment });
+  res.end();
 })
 
 router.delete("/comments", cors(), async function (req, res) {
