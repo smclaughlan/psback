0f44c64db5319ba78d7f804e7468578e2ea8a84b
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-09-17 18:17:35 -0700

    remove unnecessary comments/debug

--- app.js
+++ app.js
@@ -27,27 +27,12 @@ var app = express();
 
 // test();
 
-// const whiteList = ['http://localhost:3000/*', 'https://psstats.herokuapp.com/*'];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whiteList.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('not allowed by CORS'));
-//     }
-//   }
-// }
-
-// const cors = require('cors');
-
-// app.options('*', cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(cors());
 app.use('/', indexRouter);
 
 module.exports = app;
